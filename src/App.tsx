c8a4c28d315ac2e99b5195caf72583e33b762749
Talal Zafar <talalzafar84@gmail.com>
2025-02-15 02:54:20 +0000

    .

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <h1 className='text-red-800 bg-blue-600'>Helo</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
